56d1ada4162a8b714d408b60c9f06e70fe9bb375
Gabriel Tavares <tavaresgabriel963@gmail.com>
2021-05-31 16:59:33 -0400

    ClientResource

---
+++ backend/src/main/java/com/devsuperior/crudclientes/resources/ClientResource.java
@@ -0,0 +1,25 @@
+package com.devsuperior.crudclientes.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.crudclientes.entities.Client;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientResource {
+	
+	@GetMapping
+	public ResponseEntity<List<Client>> findAll(){
+		List<Client> list = new ArrayList<>();
+		list.add(new Client(1L, "Gabriel"));
+		list.add(new Client(2L, "João"));
+		return ResponseEntity.ok().body(list);
+	}
+
+}
