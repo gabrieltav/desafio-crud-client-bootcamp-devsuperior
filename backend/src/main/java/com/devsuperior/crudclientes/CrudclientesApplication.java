9feb95ef97856699a97e70878332a29c5e4a7f74
Gabriel Tavares <tavaresgabriel963@gmail.com>
2021-05-31 16:03:05 -0400

    Project created

---
+++ backend/src/main/java/com/devsuperior/crudclientes/CrudclientesApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.crudclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudclientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudclientesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/crudclientes/CrudclientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.crudclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudclientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
